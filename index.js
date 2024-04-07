849f9bfae97dae9367f06258fdd2fdcd8100f8ac
Julius Gutierrez <juliusbermudez03@gmail.com>
2024-04-07 15:50:21 -0700

    working api calls

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+require('dotenv').config();
 const express = require("express");
+const axios = require('axios')
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,8 +14,22 @@ app.set("view engine", "ejs");
 
 // routers
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {weather: null});
 });
+
+app.post('/', async (req,res) => {
+  const postalCode = req.body.postalCode;
+  const url = `${process.env.BASE_URL}?key=${process.env.API_KEY}&q=${postalCode}&days=7`
+
+  try {
+    const response = await axios.get(url);
+    const weather = response.data;
+    console.log(weather);
+    res.render('index', {weather: weather, error: null });
+  } catch (error) {
+    res.render('index', { weather: null, error: "Error fetching weather" });
+  }
+})
 
 // server listening
 app.listen(PORT, () => {
